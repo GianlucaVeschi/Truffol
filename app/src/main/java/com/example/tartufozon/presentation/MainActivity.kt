c6a68faf2240993a294331abd51a3e361371690b
g.veschi <g.veschi@thalia.de>
2021-01-06 23:23:22 +0100

    Attempt creating a bottom nav bar ma porcodio non viene

--- app/src/main/java/com/example/tartufozon/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tartufozon/presentation/MainActivity.kt
@@ -2,18 +2,30 @@ package com.example.tartufozon.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.tartufozon.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
 
-    companion object {
-        private const val TAG = "MainActivity"
+    fun setBottomNav() {
+        // Finding the Navigation Controller
+        var navController = findNavController(R.id.main_nav_host_fragment)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        bottomNavigation.setupWithNavController(navController)
     }
 }
+
---
+++ app/src/main/java/com/example/tartufozon/presentation/ui/profile/ProfileFragment.kt
@@ -0,0 +1,20 @@
+package com.example.tartufozon.presentation.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TruffleDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
