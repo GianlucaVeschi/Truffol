76e5b453b34dfb17d0cc13d199361f48a3cc77bb
g.veschi <g.veschi@thalia.de>
2021-02-18 08:53:54 +0100

    Adjust dependencies in Shimmer animations

--- app/src/main/java/com/example/tartufozon/presentation/components/shimmer/LoadingDetailShimmer.kt
+++ app/src/main/java/com/example/tartufozon/presentation/components/shimmer/LoadingDetailShimmer.kt
@@ -1,9 +1,11 @@
 package com.example.tartufozon.presentation.components.shimmer
 
+import androidx.compose.animation.core.*
 import androidx.compose.animation.transition
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -21,24 +23,38 @@ import androidx.compose.ui.unit.dp
 fun LoadingDetailShimmer(
     imageHeight: Dp,
     padding: Dp = 16.dp
-){
-    WithConstraints(
+) {
+    BoxWithConstraints(
         modifier = Modifier.fillMaxSize()
     ) {
-        val cardWidthPx = with(AmbientDensity.current) { (maxWidth - (padding*2)).toPx() }
+        val cardWidthPx = with(AmbientDensity.current) { (maxWidth - (padding * 2)).toPx() }
         val cardHeightPx = with(AmbientDensity.current) { (imageHeight - padding).toPx() }
+        val gradientWidth: Float = (0.2f * cardHeightPx)
 
-        val cardAnimationDefinition = remember{
-            ShimmerAnimationDefinitions(
-                widthPx = cardWidthPx,
-                heightPx = cardHeightPx,
+        val infiniteTransition = rememberInfiniteTransition()
+        val xCardShimmer = infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = (cardWidthPx + gradientWidth),
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 1300,
+                    easing = LinearEasing,
+                    delayMillis = 300
+                ),
+                repeatMode = RepeatMode.Restart
             )
-        }
-
-        val cardShimmerTranslateAnim = transition(
-            definition = cardAnimationDefinition.shimmerTranslateAnimation,
-            initState = ShimmerAnimationDefinitions.AnimationState.START,
-            toState = ShimmerAnimationDefinitions.AnimationState.END
+        )
+        val yCardShimmer = infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = (cardHeightPx + gradientWidth),
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 1300,
+                    easing = LinearEasing,
+                    delayMillis = 300
+                ),
+                repeatMode = RepeatMode.Restart
+            )
         )
 
         val colors = listOf(
@@ -46,63 +62,62 @@ fun LoadingDetailShimmer(
             Color.LightGray.copy(alpha = .3f),
             Color.LightGray.copy(alpha = .9f),
         )
-        val xCardShimmer = cardShimmerTranslateAnim[cardAnimationDefinition.xShimmerPropKey]
-        val yCardShimmer = cardShimmerTranslateAnim[cardAnimationDefinition.yShimmerPropKey]
-
-        ScrollableColumn {
-            val brush = Brush.linearGradient(
-                colors,
-                start = Offset(xCardShimmer - cardAnimationDefinition.gradientWidth, yCardShimmer - cardAnimationDefinition.gradientWidth),
-                end = Offset(xCardShimmer, yCardShimmer)
-            )
-            Surface(
-                shape = MaterialTheme.shapes.small,
-            ) {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredSize(imageHeight)
-                        .background(brush = brush)
+        LazyColumn {
+            item {
+                val brush = Brush.linearGradient(
+                    colors,
+                    start = Offset(xCardShimmer.value - gradientWidth, yCardShimmer.value - gradientWidth),
+                    end = Offset(xCardShimmer.value, yCardShimmer.value)
                 )
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Surface(
-                shape = MaterialTheme.shapes.small,
-                modifier = Modifier
-                    .padding(vertical = 8.dp)
-            ) {
-                Spacer(
+                Surface(
+                    shape = MaterialTheme.shapes.small,
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredSize(imageHeight)
+                            .background(brush = brush)
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.small,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredHeight(imageHeight / 10)
-                        .background(brush = brush)
-                )
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Surface(
-                shape = MaterialTheme.shapes.small,
-                modifier = Modifier
-                    .padding(vertical = 8.dp)
-            ) {
-                Spacer(
+                        .padding(vertical = 8.dp)
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(imageHeight / 10)
+                            .background(brush = brush)
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.small,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredHeight(imageHeight / 10)
-                        .background(brush = brush)
-                )
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Surface(
-                shape = MaterialTheme.shapes.small,
-                modifier = Modifier
-                    .padding(vertical = 8.dp)
-            ) {
-                Spacer(
+                        .padding(vertical = 8.dp)
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(imageHeight / 10)
+                            .background(brush = brush)
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.small,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredHeight(imageHeight / 10)
-                        .background(brush = brush)
-                )
+                        .padding(vertical = 8.dp)
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(imageHeight / 10)
+                            .background(brush = brush)
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/example/tartufozon/presentation/components/shimmer/LoadingListShimmer.kt
+++ app/src/main/java/com/example/tartufozon/presentation/components/shimmer/LoadingListShimmer.kt
@@ -1,11 +1,18 @@
 package com.example.tartufozon.presentation.components.shimmer
 
+import androidx.compose.animation.core.*
 import androidx.compose.animation.transition
 import androidx.compose.foundation.ScrollableColumn
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.WithConstraints
 import androidx.compose.ui.platform.AmbientDensity
@@ -13,27 +20,41 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoadingListShimmer(
+fun LoadingRecipeShimmer(
     imageHeight: Dp,
     padding: Dp = 16.dp
-){
-    WithConstraints(
+) {
+    BoxWithConstraints(
         modifier = Modifier.fillMaxSize()
     ) {
-        val cardWidthPx = with(AmbientDensity.current) { (maxWidth - (padding*2)).toPx() }
+        val cardWidthPx = with(AmbientDensity.current) { (maxWidth - (padding * 2)).toPx() }
         val cardHeightPx = with(AmbientDensity.current) { (imageHeight - padding).toPx() }
+        val gradientWidth: Float = (0.2f * cardHeightPx)
 
-        val cardAnimationDefinition = remember{
-            ShimmerAnimationDefinitions(
-                widthPx = cardWidthPx,
-                heightPx = cardHeightPx,
+        val infiniteTransition = rememberInfiniteTransition()
+        val xCardShimmer = infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = (cardWidthPx + gradientWidth),
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 1300,
+                    easing = LinearEasing,
+                    delayMillis = 300
+                ),
+                repeatMode = RepeatMode.Restart
             )
-        }
-
-        val cardShimmerTranslateAnim = transition(
-            definition = cardAnimationDefinition.shimmerTranslateAnimation,
-            initState = ShimmerAnimationDefinitions.AnimationState.START,
-            toState = ShimmerAnimationDefinitions.AnimationState.END
+        )
+        val yCardShimmer = infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = (cardHeightPx + gradientWidth),
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 1300,
+                    easing = LinearEasing,
+                    delayMillis = 300
+                ),
+                repeatMode = RepeatMode.Restart
+            )
         )
 
         val colors = listOf(
@@ -41,24 +62,65 @@ fun LoadingListShimmer(
             Color.LightGray.copy(alpha = .3f),
             Color.LightGray.copy(alpha = .9f),
         )
-        val xCardShimmer = cardShimmerTranslateAnim[cardAnimationDefinition.xShimmerPropKey]
-        val yCardShimmer = cardShimmerTranslateAnim[cardAnimationDefinition.yShimmerPropKey]
-
-        ScrollableColumn {
-            repeat(5){
-                ShimmerTruffleCardItem(
-                    colors = colors,
-                    xShimmer = xCardShimmer,
-                    yShimmer = yCardShimmer,
-                    cardHeight = imageHeight,
-                    gradientWidth = cardAnimationDefinition.gradientWidth,
-                    padding = padding
+        LazyColumn {
+            item {
+                val brush = Brush.linearGradient(
+                    colors,
+                    start = Offset(xCardShimmer.value - gradientWidth, yCardShimmer.value - gradientWidth),
+                    end = Offset(xCardShimmer.value, yCardShimmer.value)
                 )
+                Surface(
+                    shape = MaterialTheme.shapes.small,
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredSize(imageHeight)
+                            .background(brush = brush)
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.small,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(imageHeight / 10)
+                            .background(brush = brush)
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.small,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(imageHeight / 10)
+                            .background(brush = brush)
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.small,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(imageHeight / 10)
+                            .background(brush = brush)
+                    )
+                }
             }
         }
     }
-
-
 }
 
 
